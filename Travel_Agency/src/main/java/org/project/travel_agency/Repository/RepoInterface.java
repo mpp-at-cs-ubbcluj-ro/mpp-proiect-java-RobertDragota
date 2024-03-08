df17f4cdcf79cd6ab4bf6c871bec575c75cdc25f
RobertDragota <dragotarobert10@gmail.com>
2024-03-08 15:47:04 +0200

    commit

--- Travel_Agency/src/main/java/org/project/travel_agency/Repository/RepoInterface.java
+++ Travel_Agency/src/main/java/org/project/travel_agency/Repository/RepoInterface.java
@@ -1,4 +1,13 @@
 package org.project.travel_agency.Repository;
 
-public interface RepoInterface {
+import org.project.travel_agency.Domain.Entity;
+
+import java.util.Optional;
+
+public interface RepoInterface<ID,E extends Entity<ID>> {
+    Optional<E> add(E e);
+    Optional<E> update(E e);
+    Optional<E> delete(E e);
+    Optional<E> find(ID id);
+    Iterable<E> getAll();
 }
